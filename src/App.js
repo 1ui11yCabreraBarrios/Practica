b1dacf9f2a381756697f9b6e7581a20623b0093a
luillyCabreraBarrios <4lbertcabrerab@gmail.com>
2021-10-18 17:44:46 -0600

    Se creo myPage que ahora es la pagina de la app sin context y se elimino en app.js

--- src/App.js
+++ src/App.js
@@ -19,87 +19,23 @@ import moduleStyle from "../src/Estilos/Estilos.module.css"
 import Header from "./SinContext/Header";
 import Footer from "./SinContext/Footer";
 import Main from "./SinContext/Main";
+import MyPage from "./SinContext/MyPage";
 
-const initialTheme = "light";
-const initialLanguage="en";
-const initialAuth = null;;
-
-const translations={
-  es: {
-    headerTitle: "Mi aplicación SIN Context API",
-    headerSubtitle: "Mi cabecera",
-    headerLight: "Claro",
-    headerDark: "Oscuro",
-    buttonLogin: "Iniciar Sesión",
-    buttonLogout: "Cerrar Sesión",
-    mainWelcome: "Bienvenid@ invitad@",
-    mainHello: "Hola Usuari@",
-    mainContent: "Mi contenido principal",
-    footerTitle: "Mi pié de página",
-  },
-  en: {
-    headerTitle: "My application without Context API",
-    headerSubtitle: "My header",
-    headerLight: "Light",
-    headerDark: "Dark",
-    buttonLogin: "Login",
-    buttonLogout: "Logout",
-    mainWelcome: "Welcome Guest",
-    mainHello: "Hello User",
-    mainContent: "My main content",
-    footerTitle: "My footer",
-  },
-
-};
 
 
 
 function App() {
 
   const [valor, setValor] = useState(0);
-  const [theme, setTheme] = useState(initialTheme);
-  const [language, setLanguage] = useState(initialLanguage);
-  const [texts, setTexts] = useState(translations[language]);
-  const [auth, setAuth] = useState(initialAuth);;
-  console.log(texts)
-
-  const handleTheme=(e)=>{
-    console.log(e.target.value)
-    if(e.target.value ==="light"){
-      setTheme("light")
-    }else{
-      setTheme("dark")
-    }
-  }
-
-  const handleLanguage=(e)=>{
-    console.log(e.target.value)
-    if(e.target.value ==="es"){
-      setLanguage("es");
-      setTexts(translations.es)
-        }else{
-          setLanguage("en");
-      setTexts(translations.en)
-    }
-
-  }
-  const handleAuth = (e) => {
-    if (auth) {
-      setAuth(null);
-    } else {
-      setAuth(true);
-    }
-  };
+  
 
   const sumar = () => setValor(valor + 1);
   const restar = () => setValor(valor - 1)
   return (
-    <div className="my-Page">
-      <Header theme={theme} handleTheme={handleTheme} texts={texts} handleLanguage={handleLanguage}    auth={auth}
-        handleAuth={handleAuth}/>
-      <Main theme={theme } texts={texts} auth={auth}/>
-
-      <Footer theme={theme} texts={texts}/>
+    <div className="App">
+       <h1 className={moduleStyle.success}>Aplicacion Sin Context </h1>
+      <MyPage/>
+     
       <br />
       <Memorizacion />
       <h1 className={moduleStyle.success}>Agregando Rutas Crud de busqueda de canciones y almacenar en localStorage</h1>
